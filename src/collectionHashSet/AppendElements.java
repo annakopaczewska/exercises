38553ebb96fea9af6bc414caf707e144337d9ad3
annakopaczewska <anna.maria.kopaczewska@gmail.com>
2020-04-25 17:30:36 +0200

    exercises

---
+++ src/collectionHashSet/AppendElements.java
@@ -0,0 +1,24 @@
+package collectionHashSet;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class AppendElements {
+    public static void main(String[] args) {
+        HashSet<String> hashSet = new HashSet<String>();
+        hashSet.add("black");
+        hashSet.add("red");
+        hashSet.add("white");
+
+        System.out.println(hashSet);
+
+        Iterator<String> hashSet2 = hashSet.iterator();
+        for (String s : hashSet) {
+            System.out.println(hashSet2);
+        }
+        System.out.println(hashSet.size());
+
+        hashSet.removeAll(hashSet);
+        System.out.println(hashSet);
+    }
+}
