12da7256493d9cf247f1e3cb1c126557d589a39b
annakopaczewska <anna.maria.kopaczewska@gmail.com>
2020-04-27 15:28:14 +0200

    exercises

---
+++ src/collectionHashMap/KeyAndValue.java
@@ -0,0 +1,18 @@
+package collectionHashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyAndValue {
+    public static void main(String[] args) {
+        HashMap<Integer, String> myHashMap = new HashMap<Integer, String>();
+        myHashMap.put(1, "white");
+        myHashMap.put(2, "yellow");
+        myHashMap.put(3, "pink");
+        myHashMap.put(4, "black");
+
+        for (Map.Entry a : myHashMap.entrySet()) {
+            System.out.println(a.getKey() + " " + a.getValue());
+        }
+    }
+}
