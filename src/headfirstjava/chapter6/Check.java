38a0e97beee53a090804c9d7d916001d6026447c
annakopaczewska <anna.maria.kopaczewska@gmail.com>
2020-04-30 12:19:52 +0200

    exercises

--- src/headfirstjava/chapter6/Check.java
+++ src/headfirstjava/chapter6/Check.java
@@ -5,22 +5,27 @@ import java.util.ArrayList;
 public class Check {
     static class EasyGame {
         private ArrayList positionField;
+        private String name;
 
         public void setPositionField(ArrayList pf) {
             positionField = pf;
         }
 
+        public void setName(String nameGame) {
+            name = nameGame;
+        }
+
         public String check(String move) {
             String result = "mishit";
             int index = positionField.indexOf(move);
-
             if (index >= 0) {
                 positionField.remove(index);
-            }
-            if (positionField.isEmpty()) {
-                result = "sunk";
-            } else {
-                result = "hit!";
+                if (positionField.isEmpty()) {
+                    result = "sunk";
+                    System.out.println("Aghh! You Ssunk game! " + name + " :( ");
+                } else {
+                    result = "hit!";
+                }
             }
             return result;
         }
