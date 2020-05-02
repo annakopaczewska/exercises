9f2991a108775ab9ba5bd09996f0ce8550ac9053
annakopaczewska <anna.maria.kopaczewska@gmail.com>
2020-05-02 21:30:20 +0200

    exercises

---
+++ src/exBasicI/RandomInteger.java
@@ -0,0 +1,13 @@
+package exBasicI;
+
+import java.util.Random;
+
+public class RandomInteger {
+    public static void main(String[] args) {
+        System.out.println(randomNumb(25, 5));
+    }
+
+    public static int randomNumb(int max, int min) {
+        return min + (int) (Math.random() * ((max - min) + 1));
+    }
+}
