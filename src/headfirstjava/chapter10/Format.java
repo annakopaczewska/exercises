48dba9e419ed74dce3007b49ca323bdcb7a035c2
annakopaczewska <anna.maria.kopaczewska@gmail.com>
2020-05-06 19:13:10 +0200

    exercises

--- src/headfirstjava/chapter10/Format.java
+++ src/headfirstjava/chapter10/Format.java
@@ -1,6 +1,7 @@
 package headfirstjava.chapter10;
 
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Format {
     public static void main(String[] args) {
@@ -9,5 +10,11 @@ public class Format {
         NumberFormat format = NumberFormat.getCurrencyInstance();
         String myString = format.format(a);
         System.out.println(myString);
+
+
+        Locale locale = Locale.UK;
+        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(locale);
+        String string = numberFormat.format(a);
+        System.out.println(string);
     }
 }
