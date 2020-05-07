e6ba6b52b76de2568227599c0ca927879d99c201
annakopaczewska <anna.maria.kopaczewska@gmail.com>
2020-05-07 09:27:51 +0200

    exercises

--- src/headfirstjava/chapter11/MIDI.java
+++ src/headfirstjava/chapter11/MIDI.java
@@ -5,7 +5,7 @@ import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequencer;
 
 public class MIDI {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MidiUnavailableException {
         MIDI musicTest = new MIDI();
         musicTest.play();
     }
