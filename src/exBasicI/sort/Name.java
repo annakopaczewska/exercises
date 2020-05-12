961f7157eb8f4078665e0474546cbfb1dcaf0e90
annakopaczewska <anna.maria.kopaczewska@gmail.com>
2020-05-12 14:43:42 +0200

    exercises

--- src/exBasicI/sort/Name.java
+++ src/exBasicI/sort/Name.java
@@ -2,6 +2,7 @@ package exBasicI.sort;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Name {
@@ -13,9 +14,13 @@ public class Name {
         list.add(new Person("Stanisław", "Wojtala", 86));
         list.add(new Person("Anna", "Nara", 43));
 
-//        Collections.sort(list);
+//        Collections.sort(list, new PersonAgeComparator().reversed());
+//        System.out.println(list);
 
-        Collections.sort(list, new PersonAgeComparator().reversed());
+        Comparator<Person> c = Comparator.comparingInt(Person::getAge);
+        Comparator<Person> reversed = c.reversed();
+
+        Collections.sort(list,reversed);
         System.out.println(list);
     }
 }
