3a39a95df5505c345536eb33fb68277816f6c73a
annakopaczewska <anna.maria.kopaczewska@gmail.com>
2020-05-12 10:38:24 +0200

    exercises

--- src/exBasicI/Compare.java
+++ src/exBasicI/Compare.java
@@ -2,6 +2,8 @@ package exBasicI;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Consumer;
 
 
 public class Compare {
@@ -17,31 +19,39 @@ public class Compare {
         names.add("Stanisław");
         names.add("Ola");
 
-        names = filter(names, () -> false);
-        modifyAndDisplay(names, n -> n + " " + "!");
+        names.stream()
+                .filter(s -> s.endsWith("a"))
+                .map(name -> name + "!")
+                .forEach(Compare::print);
+//        names = filter(names, () -> false);
+//        modifyAndDisplay(names, n -> n + " " + "!");
     }
 
-    private List<String> filter(List<String> input, Filter filter) {
-        List<String> resultList = new ArrayList<>();
-        for (String name : input) {
-            if (filter.filter()) {
-                resultList.add(name);
-            }
-        }
-        return resultList;
+    private static void print(String a) {
+        System.out.println(a);
     }
 
-    private void modifyAndDisplay(List<String> names, Modifier modifier) {
-        List<String> modifiedNames = new ArrayList<>();
-
-        for (String name : names) {
-            String modifiedName = modify(name, modifier);
-            modifiedNames.add(modifiedName);
-        }
-        System.out.println(modifiedNames);
-    }
-
-    private String modify(String string, Modifier modifier) {
-        return modifier.modify(string);
-    }
+//    private List<String> filter(List<String> input, Filter filter) {
+//        List<String> resultList = new ArrayList<>();
+//        for (String name : input) {
+//            if (filter.filter()) {
+//                resultList.add(name);
+//            }
+//        }
+//        return resultList;
+//    }
+//
+//    private void modifyAndDisplay(List<String> names, Modifier modifier) {
+//        List<String> modifiedNames = new ArrayList<>();
+//
+//        for (String name : names) {
+//            String modifiedName = modify(name, modifier);
+//            modifiedNames.add(modifiedName);
+//        }
+//        System.out.println(modifiedNames);
+//    }
+//
+//    private String modify(String string, Modifier modifier) {
+//        return modifier.modify(string);
+//    }
 }
